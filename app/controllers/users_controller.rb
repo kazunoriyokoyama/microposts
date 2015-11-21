8aa92deb32f1e84bab467d15d3ffda93770fa381
kazunoriyokoyama <info@ultrac.co.jp>
2015-11-21 09:45:07 +0000

    1l21

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user =User.new(user_params)
+    if @user.save
+       flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :area, :password, :password_confrimation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  
+end
