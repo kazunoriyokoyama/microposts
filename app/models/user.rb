eb0f8422f3be800fbae76b7bbbabe63c4e2873e9
kazunoriyokoyama <info@ultrac.co.jp>
2015-11-21 12:35:28 +0000

    create5

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
      before_save { self.email = email.downcase }
      validates :name, presence: true, length: { maximum: 50 }
      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-     validates :email, presence: true, length: { maximum: 255 },
-                       format: { with: VALID_EMAIL_REGEX },
-                       uniqueness: { case_sensitive: false }
+     validates :email, presence: true, length: { maximum: 255 }, on: :create,
+                       format: { with: VALID_EMAIL_REGEX }, on: :create,
+                       uniqueness: { case_sensitive: false }, on: :create
+     validates :email, presence: true, on: :update
+                       
      validates :area, presence: true               
      has_secure_password
 end
